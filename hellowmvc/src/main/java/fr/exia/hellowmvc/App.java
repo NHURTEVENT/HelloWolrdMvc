09d550a2bbf8ee178b45931de0c725f8f7994634
LordRassilon <TheLordrassilon@gmail.com>
2017-06-07 14:13:20 +0200

    phase1

---
+++ hellowmvc/src/main/java/fr/exia/hellowmvc/App.java
@@ -0,0 +1,13 @@
+package fr.exia.hellowmvc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
